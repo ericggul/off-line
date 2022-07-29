28ca0da88f7969a2954b46a074100e888c203e1d
ericggul <57379903+ericggul@users.noreply.github.com>
2022-07-29 23:19:15 +0900

    Feat: Oasis

--- src/containers/Oasis/index.js
+++ src/containers/Oasis/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import * as S from './styles';
+import React, { useMemo, useEffect, useState } from "react";
+import * as S from "./styles";
+
+import * as Tone from "tone";
+
+const getRandom = (a, b) => Math.random() * (b - a) + a;
 
 function Oasis() {
+  const CHORD_ARRAY = ["B5", "C6", "E6", "F#6", "G6"];
+  const [chordIdx, setChordIdx] = useState(0);
+
+  function playSynth() {
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    const now = Tone.now();
+    const playStart = getRandom(0, 2);
+    const playEnd = getRandom(3, 5);
+    synth.triggerAttack(CHORD_ARRAY[chordIdx], now + playStart);
+    synth.triggerRelease(CHORD_ARRAY[chordIdx], now + playEnd);
+    setChordIdx((pl) => (1 + pl) % CHORD_ARRAY.length);
+  }
+
   return (
-    <S.StyledOasis>
-      Oasis
+    <S.StyledOasis onClick={playSynth}>
+      <h1>Oasis</h1>
+      <p>Opening Soon</p>
     </S.StyledOasis>
   );
 }
--- src/containers/Oasis/styles.js
+++ src/containers/Oasis/styles.js
@@ -1,5 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { FlexCenterStyle, WholeContainer } from "static/styles";
 
 export const StyledOasis = styled.div`
+  ${WholeContainer};
+  ${FlexCenterStyle};
+  flex-direction: column;
+  position: relative;
 
+  font-family: Dior;
+
+  letter-spacing: -0.07rem;
+
+  h1 {
+    font-size: 4rem;
+    font-weight: bold;
+  }
+
+  p {
+    margin: 0;
+    font-size: 1.5rem;
+  }
 `;
